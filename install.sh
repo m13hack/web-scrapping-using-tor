57e488a2159819b5bd4f80c06fb28eb89b05665c
m13hack <119548847+m13hack@users.noreply.github.com>
2024-07-31 18:12:31 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ sudo apt-get update
 # Install Python3, pip, and Tor if they are not already installed
 sudo apt-get install -y python3 python3-pip tor
 
-# Install Python dependencies
-pip3 install httpx beautifulsoup4
+# Install Python dependencies including SOCKS support for httpx
+pip3 install httpx[socks] beautifulsoup4
 
 # Check if Tor service is installed and start it if necessary
 if ! pgrep -x "tor" > /dev/null
