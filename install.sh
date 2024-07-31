a90aa837357c9f2ca4fe05cf7bb435e9d2f9944c
m13hack <119548847+m13hack@users.noreply.github.com>
2024-07-31 17:33:08 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@
 # Update package lists
 sudo apt-get update
 
-# Install Python3 and pip if they are not already installed
+# Install Python3, pip, and Tor if they are not already installed
 sudo apt-get install -y python3 python3-pip tor
 
 # Install Python dependencies
-pip3 install requests beautifulsoup4 stem
+pip3 install scrapfly httpx
 
-# Check if Tor service is installed
+# Check if Tor service is installed and start it if necessary
 if ! pgrep -x "tor" > /dev/null
 then
     echo "Tor is not running. Starting Tor service..."
